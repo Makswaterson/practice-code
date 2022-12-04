a5bb25416d7464efda3dbd38d0c31d312c5d5563
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-04 23:06:30 +0200

    finish 12

--- Lesson_12/script.js
+++ Lesson_12/script.js
@@ -33,21 +33,82 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 
-const form = document.querySelector('.js-form');
-form.addEventListener('submit', onSubmit);
+// const form = document.querySelector('.js-form');
+// form.addEventListener('submit', onSubmit);
 
-function onSubmit(evt) {
-  evt.preventDefault();
-  //   const data = new FormData(evt.currentTarget);
-  //   console.log(data);
-  //   data.forEach(value => console.log(value));
-  const {
-    gender: { value: gender },
-    userName: { value: userName },
-  } = evt.currentTarget.elements;
-  console.log('gender', gender);
-  console.log('userName', userName);
-  // const {gender, userName} = evt.currentTarget.elements;
-  //     // console.log('userName',userName.value);
-  //     // console.log('gender',gender.value);
+// function onSubmit(evt) {
+//   evt.preventDefault();
+//   const data = new FormData(evt.currentTarget);
+//   console.log(data);
+//   data.forEach(value => console.log(value));
+// const {
+//   gender: { value: gender },
+//   userName: { value: userName },
+// } = evt.currentTarget.elements;
+// console.log('gender', gender);
+// console.log('userName', userName);
+// const {gender, userName} = evt.currentTarget.elements;
+//     // console.log('userName',userName.value);
+//     // console.log('gender',gender.value);
+// }
+
+//////////////////////////////////////Хрестики-нулики ...............................
+
+const content = document.querySelector('.content');
+let markup = '';
+let stepX = [];
+let stepO = [];
+const win = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+let player = 'X';
+for (let i = 1; i <= 9; i++) {
+  markup += `<div class = 'item' data-id='${i}'></div>`;
+}
+content.insertAdjacentHTML('beforeend', markup);
+
+content.addEventListener('click', onClickGameZone);
+
+function onClickGameZone(evt) {
+  if (!evt.target.textContent) {
+    const id = Number(evt.target.dataset.id);
+    if (player == 'X') {
+      stepX.push(id);
+      const isWinner = checkWinner(stepX);
+      if (isWinner) {
+        console.log(`${player} is Winner!`);
+        reset();
+        return;
+      }
+    } else {
+      stepO.push(id);
+      const isWinner = checkWinner(stepO);
+      if (isWinner) {
+        console.log(`${player} is Winner!`);
+        reset();
+        return;
+      }
+    }
+    evt.target.textContent = player;
+    player = player === 'X' ? 'O' : 'X';
+  }
+}
+
+function checkWinner(arr) {
+  const result = win.some(values => values.every(value => arr.includes(value)));
+  return result;
+}
+
+function reset() {
+  content.innerHTML = markup;
+  player = 'X';
+  stepX = [];
+  stepO = [];
 }
