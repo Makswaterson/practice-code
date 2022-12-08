d63cae62ed7e4ba7d6dd679bb45b7c6ecaa4c4a3
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-08 20:18:17 +0200

    prepared lesson 13

---
+++ Lesson_13/script.js
@@ -0,0 +1,20 @@
+const instruments = [
+  {
+    id: 1,
+    img: 'https://static.dnipro-m.ua/cache/products/1754/catalog_origin_141546.jpg',
+    name: 'Молоток',
+    price: 150,
+  },
+  {
+    id: 2,
+    img: 'https://static.dnipro-m.ua/cache/products/5098/catalog_origin_195568.jpg',
+    name: 'Перфоратор',
+    price: 3000,
+  },
+  {
+    id: 3,
+    img: 'https://static.dnipro-m.ua/cache/products/2023/catalog_origin_200763.jpg',
+    name: 'Рівень',
+    price: 2000,
+  },
+];
