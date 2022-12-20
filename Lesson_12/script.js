a7d97ff1fa7e75544f21d253724cd3d6f5a0cdbe
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-21 00:10:54 +0200

    Update script.js

--- Lesson_12/script.js
+++ Lesson_12/script.js
@@ -52,6 +52,67 @@
 //     // console.log('gender',gender.value);
 // }
 
+/////////////////////////////////////////////////////////////////////////////////
+
+// const content = document.querySelector('.content');
+// let markup = '';
+// let stepX = [];
+// let stepO = [];
+// const win = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [3, 6, 9],
+//   [1, 5, 9],
+//   [3, 5, 7],
+// ];
+// let player = 'X';
+// for (let i = 1; i <= 9; i++) {
+//   markup += `<div class = 'item' data-id='${i}'></div>`;
+// }
+// content.insertAdjacentHTML('beforeend', markup);
+
+// content.addEventListener('click', onClickGameZone);
+
+// function onClickGameZone(evt) {
+//   if (!evt.target.textContent) {
+//     const id = Number(evt.target.dataset.id);
+//     if (player == 'X') {
+//       stepX.push(id);
+//       const isWinner = checkWinner(stepX);
+//       if (isWinner) {
+//         console.log(`${player} is Winner!`);
+//         reset();
+//         return;
+//       }
+//     } else {
+//       stepO.push(id);
+//       const isWinner = checkWinner(stepO);
+//       if (isWinner) {
+//         console.log(`${player} is Winner!`);
+//         reset();
+//         return;
+//       }
+//     }
+//     evt.target.textContent = player;
+//     player = player === 'X' ? 'O' : 'X';
+//   }
+// }
+
+// function checkWinner(arr) {
+//   const result = win.some(values => values.every(value => arr.includes(value)));
+//   return result;
+// }
+
+// function reset() {
+//   content.innerHTML = markup;
+//   player = 'X';
+//   stepX = [];
+//   stepO = [];
+// }
+
 //////////////////////////////////////Хрестики-нулики ...............................
 
 const content = document.querySelector('.content');
