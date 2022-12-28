b1064e5c1d4989b8ff6d69d246c29cea0a38d067
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-28 17:05:34 +0200

    racetrack add

---
+++ Lesson_18/racetrack.js
@@ -0,0 +1,15 @@
+const horses = ['Secretariat', 'Eclipse', 'West Australian', 'Flying Fox', 'Seabiscuit'];
+
+// let raceCounter = 0;
+// const refs = {
+//   startBtn: document.querySelector('.js-start-race'),
+//   winnerField: document.querySelector('.js-winner'),
+//   progressField: document.querySelector('.js-progress'),
+//   tableBody: document.querySelector('.js-results-table > tbody'),
+// };
+
+// refs.startBtn.addEventListener('click', onStart);
+
+// function getRandomTime(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
