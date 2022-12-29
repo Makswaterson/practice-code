225a6009d0624f8aa56d04de6f07ef1e77d4162f
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-29 18:38:27 +0200

    Update script.js

--- Lesson_18/script.js
+++ Lesson_18/script.js
@@ -7,3 +7,14 @@
 // 2. setInterval
 // 3. setImmediate
 // 4. requestAnimationFrame
+
+// console.log('1');
+
+// setTimeout(() => {
+//     console.log('2')
+// }, 0)
+// Promise.resolve("3").then(value => console.log(value))
+
+// console.log('4');
+
+// // Відповідь 1,4,3,2
