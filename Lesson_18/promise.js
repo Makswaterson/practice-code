555550e1c04698308cd390747e183cf9f7ca6e40
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-29 10:24:56 +0200

    Update promise.js

--- Lesson_18/promise.js
+++ Lesson_18/promise.js
@@ -11,3 +11,45 @@
  */
 
 // const getRandomNumber = () => Math.floor(Math.random() * 4) + 1;
+// const number = getRandomNumber();
+// const delay = number * 1000;
+
+// const onSuccess = delay => {
+//   console.log(`✅ Resolved after ${delay} sec`);
+// };
+// const onError = delay => {
+//   console.log(`❌ Rejected after ${delay} sec`);
+// };
+
+// function createPromise(number, delay) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (number <= 2) {
+//         resolve(number);
+//       } else {
+//         reject(number);
+//       }
+//     }, delay);
+//   });
+// }
+// createPromise(number, delay)
+//   .then(onSuccess)
+//   .catch(onError)
+//   .finally(() => {
+//     console.log('finally');
+//   });
+
+/*
+ * Methods:
+ * Promise.race(array) - повертає перший успішно виконаний або відхилений проміс, зі значенням чи причиною відхилення цього промісу.
+ *
+ * Promise.all(array) - повертає проміс, який виконається тоді, коли будуть успішно виконані всі проміси, або відхилено будь-який з них.
+ *
+ */
+
+/*
+ * Зробіть 3 проміси - по одному для кожного фреймворку з масиву.
+ * У кожному розташована функція setTimeout із випадковою затримкою від 0 до 2 секунд.
+ * Зробити так, щоб проміси і резолвилися, і реджектилися випадково.
+ * Нехай кожен проміс своїм результатом повертає цю затримку та ім'я фреймворку, а при помилці ще й текст помилки 'Promise error'.
+ */
