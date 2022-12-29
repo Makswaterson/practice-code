f45d5b04ac7d754dd6c7991e855bbb01e4c462ae
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-29 15:58:29 +0200

    create new files

---
+++ Lesson_18/script.js
@@ -0,0 +1,9 @@
+// Мікро процеси мають БІЛЬШИЙ пріорітет
+// 1. Promise
+// 2. Observer
+
+// Макро процеси мають МЕНЬШИЙ пріорітет
+// 1. setTimeout
+// 2. setInterval
+// 3. setImmediate
+// 4. requestAnimationFrame
