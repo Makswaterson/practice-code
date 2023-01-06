9e506c49da9e3737cd0fd9602aea57ac2ef6895a
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-01-07 01:55:25 +0200

    upd script.js with weather

--- Lesson_19/script.js
+++ Lesson_19/script.js
@@ -17,3 +17,70 @@
 //   .then(console.log);
 
 //////////////////////////////////////////////////////////////////////////////////////
+
+// 94a040c94bab4dcabf9130808222712
+// https://www.postman.com/downloads/
+
+// const response = fetch(`http://api.weatherapi.com/v1/forecast.json?key=94a040c94bab4dcabf9130808222712&q=Lviv&days=5`);
+// response.then(resp => resp.json()).then(data => console.log(data))
+
+const form = document.querySelector('.js-search');
+const list = document.querySelector('.js-list');
+
+const errorRequest =
+  'https://global.discourse-cdn.com/brave/original/3X/b/2/b25ce7b5ef1396e782cee4f7bbffaefd7f9d3b49.jpeg';
+const emptyValue =
+  'https://www.shutterstock.com/image-illustration/no-value-red-rubber-stamp-260nw-426853699.jpg';
+
+form.addEventListener('submit', onSearch);
+
+function onSearch(evt) {
+  evt.preventDefault();
+  const { query, days } = evt.currentTarget;
+  if (query.value) {
+    console.log(query.value, days.value);
+    weatherApi(query.value, days.value)
+      .then(data => createMarkup(data))
+      .catch(error => createErrorMessage(error, errorRequest));
+  } else {
+    createErrorMessage('Empty search value!', emptyValue);
+  }
+}
+
+function weatherApi(city, days) {
+  const BASE_URL = 'http://api.weatherapi.com/v1';
+  const key = '94a040c94bab4dcabf9130808222712';
+  return fetch(`${BASE_URL}/forecast.json?key=${key}&q=${city}&days=${days}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+function createMarkup({ forecast: { forecastday } }) {
+  const markup = forecastday.map(
+    ({
+      date,
+      day: {
+        avgtemp_c: avg,
+        condition: { text, icon },
+      },
+    }) => `<li>
+  <img src="${icon}" alt="${text}">
+    <h2>Дата:${date}</h2>
+      <h3>Температура:${avg}</h3>
+    <span>${text}</span>
+  </img>
+</li>`
+  );
+  list.innerHTML = markup.join('');
+}
+
+function createErrorMessage(error, img) {
+  const markup = `<li>
+    <h2>${error}</h2>
+    <img src="${img}" alt="${error}" width="300">
+</li>`;
+  list.innerHTML = markup;
+}
