d6e67edcf177ffb6c89b44a6d397528d88820494
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-01-08 01:11:40 +0200

    prepared les 20

---
+++ Lesson_20/index.js
@@ -0,0 +1,9 @@
+// const API_KEY = '4330ebfabc654a6992c2aa792f3173a3';
+// const BASE_URL = 'https://newsapi.org/v2';
+// const options = {
+//   headers: {
+//     Authorization: API_KEY,
+//   },
+// };
+
+fetch();
