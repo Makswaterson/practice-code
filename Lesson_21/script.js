d6e4e66dc6c184d7b7d8ad9f9462c6a10b764fa6
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-01-19 23:11:50 +0200

    prepared

---
+++ Lesson_21/script.js
@@ -0,0 +1,12 @@
+// https://dummyjson.com/docs/posts
+// // https://jsonplaceholder.typicode.com/posts
+
+// CRUD
+// C Create - POST
+// R Read   - GET
+// U Update - PUT PATCH
+// D DELETE - DELETE
+
+// R Read   - GET
+
+const BASE_URL = 'http://localhost:4040';
